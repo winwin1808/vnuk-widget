4719dec03f149cdbcc9c6f6e05b4ee74bdcd3ea0
Hao Nguyen <nguyenquanghao1808@gmail.com>
2024-06-22 16:39:28 +0700

    Handles socket user cases

--- src/context/socket.jsx
+++ src/context/socket.jsx
@@ -1,39 +1,46 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
 import io from 'socket.io-client';
 import { host } from '../utils/ApiRoutes';
 
 const SocketContext = createContext();
 
 export const useSocketContext = () => {
-    return useContext(SocketContext);
+  return useContext(SocketContext);
 };
 
 export const SocketContextProvider = ({ children }) => {
-    const [socket, setSocket] = useState(null);
+  const [socket, setSocket] = useState(null);
+  
+  useEffect(() => {
+    const customerId = localStorage.getItem('customerId');
+    if (customerId) {
+      setSocketCustomerId(customerId);
+    }
+  }, []);
+  
+  const setSocketCustomerId = (customerId) => {
+    if (customerId) {
+      // Initialize socket connection
+      const newSocket = io(host, {
+        query: {
+          userId: customerId, // Correctly set the userId as a string
+        },
+        withCredentials: true,
+      });
 
-    const setSocketCustomerId = (customerId) => {
-        if (customerId) {
-            // Initialize socket connection
-            const socket = io(host, {
-                query: {
-                    userId: customerId, // Correctly set the userId as a string
-                },
-                withCredentials: true,
-            });
+      setSocket(newSocket);
 
-            setSocket(socket);
+      // Clean up the socket when the component unmounts or customerId changes
+      return () => {
+        newSocket.close();
+        setSocket(null);
+      };
+    }
+  };
 
-            // Clean up the socket when the component unmounts or customerId changes
-            return () => {
-                socket.close();
-                setSocket(null);
-            };
-        }
-    };
-
-    return (
-        <SocketContext.Provider value={{ socket, setSocketCustomerId }}>
-            {children}
-        </SocketContext.Provider>
-    );
+  return (
+    <SocketContext.Provider value={{ socket, setSocketCustomerId }}>
+      {children}
+    </SocketContext.Provider>
+  );
 };
